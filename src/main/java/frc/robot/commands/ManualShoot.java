d45b58b326386bba88b07f7a9d13f08e68e84acc
DeceiverM <FRC4392@gmail.com>
2020-02-03 17:34:56 -0500

    Manual Shoot 2

--- src/main/java/frc/robot/commands/ManualShoot.java
+++ src/main/java/frc/robot/commands/ManualShoot.java
@@ -27,6 +27,7 @@ public class ManualShoot extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    mShooter.setVelocity();
   }
 
   // Called once the command ends or is interrupted.
