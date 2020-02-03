4f77a0b6c0771b797d84adc619345e0ab1aad9bc
DeceiverM <FRC4392@gmail.com>
2020-02-03 18:17:54 -0500

    Intake 3

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Intake;
 
 public class Intake extends CommandBase {
   public final frc.robot.subsystems.Intake mIntake;
