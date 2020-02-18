662672f983b4d207707edcc9c9ba07881ed68a1a
DeceiverM <FRC4392@gmail.com>
2020-02-18 18:13:36 -0500

    Dumb Periodic

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -108,24 +108,6 @@ public class Climber extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-
-    //Read PID coefficients from SmartDashboard
-    Double p = SmartDashboard.getNumber("P Gain", 0);
-    Double i = SmartDashboard.getNumber("I Gain", 0);
-    Double d = SmartDashboard.getNumber("D Gain", 0);
-    Double iz = SmartDashboard.getNumber("I Zone", 0);
-    Double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    Double max = SmartDashboard.getNumber("Max Output", 0);
-    Double min = SmartDashboard.getNumber("Min Output", 0);
-
-    //If PID coeffiecients on SmartDashboard have changed, write new values to controller
-    if ((p != kP)) {mPidController.setP(p); kP = p;}
-    if ((i != kI)) {mPidController.setI(i); kI = i;}
-    if ((d != kD)) {mPidController.setD(d); kD = d;}
-    if ((iz != kIz)) {mPidController.setIZone(iz); kIz = iz;}
-    if ((ff != kFF)) {mPidController.setFF(ff); kFF = ff;}
-    if ((max != kMaxOutput) || (min != kMinOutput)) {mPidController.setOutputRange(min, max); kMinOutput = min; kMaxOutput = max;}
-
+    
   }
 }
