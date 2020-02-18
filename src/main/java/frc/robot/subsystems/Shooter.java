3fae4343583435b9cb57b7962b7d97f085d6cbc2
Kristopher Zakfeld <kzakfeld@gmail.com>
2020-02-18 17:26:20 -0500

    Add HoodPosition Enum

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -27,6 +27,21 @@ public class Shooter extends SubsystemBase {
 
   private Double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
+  public enum HoodPosition{
+    Open(true),
+    Closed(false);
+
+    private final boolean solenoidState;
+
+    HoodPosition(boolean state){
+      solenoidState = state;
+    }
+
+    private boolean getSolenoidState() {
+      return solenoidState;
+    }
+  }
+
 
   /**
    * Creates a new Shooter.
@@ -36,26 +51,26 @@ public class Shooter extends SubsystemBase {
     mWheelMotor2 = new CANSparkMax(62, MotorType.kBrushless);
 
     mPidController = mWheelMotor.getPIDController();
-      mEncoder = mWheelMotor.getEncoder();
+    mEncoder = mWheelMotor.getEncoder();
 
 
-      kP = mRobotPreferences.getDouble("ShooterKP", 5e-5);
-      kI = mRobotPreferences.getDouble("ShooterKI", 1e-6);
-      kD = mRobotPreferences.getDouble("ShooterKD", 0.0);
-      kIz = mRobotPreferences.getDouble("ShooterKIz", 0.0);
-      kFF = mRobotPreferences.getDouble("ShooterKFF", 0.0);
-      kMaxOutput = mRobotPreferences.getDouble("ShooterKMaxOutput", 1.0);
-      kMinOutput = mRobotPreferences.getDouble("ShooterKMinOutput", -1.0);
-      maxRPM = 5700.0;
+    kP = mRobotPreferences.getDouble("ShooterKP", 5e-5);
+    kI = mRobotPreferences.getDouble("ShooterKI", 1e-6);
+    kD = mRobotPreferences.getDouble("ShooterKD", 0.0);
+    kIz = mRobotPreferences.getDouble("ShooterKIz", 0.0);
+    kFF = mRobotPreferences.getDouble("ShooterKFF", 0.0);
+    kMaxOutput = mRobotPreferences.getDouble("ShooterKMaxOutput", 1.0);
+    kMinOutput = mRobotPreferences.getDouble("ShooterKMinOutput", -1.0);
+    maxRPM = 5700.0;
 
-      mPidController.setP(kP);
-      mPidController.setI(kI);
-      mPidController.setD(kD);
-      mPidController.setIZone(kIz);
-      mPidController.setFF(kFF);
-      mPidController.setOutputRange(kMinOutput, kMaxOutput);
+    mPidController.setP(kP);
+    mPidController.setI(kI);
+    mPidController.setD(kD);
+    mPidController.setIZone(kIz);
+    mPidController.setFF(kFF);
+    mPidController.setOutputRange(kMinOutput, kMaxOutput);
 
-      mWheelMotor2.follow(mWheelMotor);
+    mWheelMotor2.follow(mWheelMotor);
 
   }
     public void setVelocity(double velocity) {
@@ -63,12 +78,8 @@ public class Shooter extends SubsystemBase {
 
     }
 
-    public void setHood(){
-      
-    }
-
-    public void ShooterInit() {
-      
+    public void setHood(HoodPosition position){
+      mSolenoid.set(position.getSolenoidState());
     }
     
   @Override
