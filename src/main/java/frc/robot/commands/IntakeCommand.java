eb77b721033048a68162eced0e37096089494652
DeceiverM <FRC4392@gmail.com>
2020-03-03 17:45:47 -0500

    Indexer and Intake

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -8,14 +8,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 
 public class IntakeCommand extends CommandBase {
   public final Intake mIntake;
+  public final Indexer mIndexer;
 
-  public IntakeCommand(frc.robot.subsystems.Intake Intake) {
+  public IntakeCommand(frc.robot.subsystems.Intake Intake, frc.robot.subsystems.Indexer Indexer) {
     mIntake = Intake;
     addRequirements(mIntake);
+
+    mIndexer = Indexer;
+    addRequirements(mIndexer);
   }
 
   // Called when the command is initially scheduled.
@@ -27,12 +32,14 @@ public class IntakeCommand extends CommandBase {
   @Override
   public void execute() {
     mIntake.setSpeed(1.0);
+    mIndexer.setSpeed(1.0);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     mIntake.setSpeed(0.0);
+    mIndexer.setSpeed(0.0);
   }
 
   // Returns true when the command should end.
